53d1aa232144ef275b5757959aa31c41b15ced69
cesilko1 <wilhelm.master@gmail.com>
2020-04-02 16:21:00 +0200

    saving to json file

--- src/getRoundedTime.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from datetime import datetime
-from datetime import timedelta
-
-def getRoundedTime(dt, delta):
-	return dt + (datetime.min - dt) % delta
--- src/start.py
+++ src/start.py
@@ -7,18 +7,53 @@ start instance
 from datetime import datetime
 from sys import argv
 import json
+import sys
 
 
 jsonFileName = "counting.json"
 
 startTime = datetime.now()
 
+
+def newData(instance, csvFileName):
+	instance = str(instance)
+	csvFileName = str(csvFileName)
+
+	newString = '{"'+instance+'":{"start-time":"'+str(startTime)+'", "csv-path":"'+csvFileName+'"} }'
+	newJsonData = json.loads(newString)
+
+	return newJsonData
+
+
+
+
+
 with open(jsonFileName) as f:
 	jsonString = f.read()
-	jsonData = json.loads(jsonString)
+	try:
+		jsonData = json.loads(jsonString)
+	except:
+		print("error while loading json")
+		jsonData = newData(argv[1], argv[2])
+
 
 
 for instance in jsonData:
+	
 	if instance == argv[1]:
-		if jsonData[instance]["running"] == True:
-			return
+		#json instance exists
+		print("this job is already running!")
+		sys.exit()
+
+	else:
+		print("adding new instance")
+		jsonData.update(newData(argv[1], argv[2]))
+
+
+
+with open(jsonFileName, "w") as f:
+	print("saving json")
+	newString = json.dumps(jsonData)
+	f.write(newString)
+
+
