95bd11b1cddb211404b28fc71c9671deb695bcdc
cesilko1 <wilhelm.master@gmail.com>
2020-04-02 17:08:52 +0200

    saving to json fixed

--- src/start.py
+++ src/start.py
@@ -14,16 +14,18 @@ jsonFileName = "counting.json"
 
 startTime = datetime.now()
 
+exists = False
+instance_name = ""
 
-def newData(instance, csvFileName):
+
+def newData(instance, csvFileName, running):
 	instance = str(instance)
 	csvFileName = str(csvFileName)
 
-	newString = '{"'+instance+'":{"start-time":"'+str(startTime)+'", "csv-path":"'+csvFileName+'"} }'
+	newString = '{"'+instance+'":{"start-time":"'+str(startTime)+'", "csv-path":"'+csvFileName+'", "running":"'+str(running)+'"} }'
 	newJsonData = json.loads(newString)
 
 	return newJsonData
-
 
 
 
@@ -33,27 +35,27 @@ with open(jsonFileName) as f:
 	try:
 		jsonData = json.loads(jsonString)
 	except:
-		print("error while loading json")
-		jsonData = newData(argv[1], argv[2])
+		jsonData = newData(argv[1], argv[2], True)
 
 
 
 for instance in jsonData:
-	
 	if instance == argv[1]:
-		#json instance exists
-		print("this job is already running!")
-		sys.exit()
+		exists = True
+		instance_name = instance
+		break
 
+
+if exists:
+	if jsonData[instance_name]["running"] == "False":
+		jsonData[instance_name]["start-time"] = str(startTime)
+		jsonData[instance_name]["running"] = True
 	else:
-		print("adding new instance")
-		jsonData.update(newData(argv[1], argv[2]))
+		pass
 
-
+else:
+	jsonData.update(newData(argv[1], argv[2], True))
 
 with open(jsonFileName, "w") as f:
-	print("saving json")
 	newString = json.dumps(jsonData)
 	f.write(newString)
-
-
--- src/time-counter.sh
+++ src/time-counter.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+#cd ~/.working-time-counter
+
+case $1 in
+	start)
+		python3 start.py $2 $3
+		;;
+	stop)
+		python3 stop.py $2
+		;;
+	help)
+		cat ../help.txt
+		;;
+	*)
+		cat ../help.txt
+		;;
+esac
