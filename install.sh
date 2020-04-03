e1365e4f79bdd2749d26b85030854d8cf65fce88
cesilko1 <wilhelm.master@gmail.com>
2020-04-03 22:34:46 +0200

    installing fixed

--- install.sh
+++ install.sh
@@ -2,22 +2,33 @@
 
 if [ -f /usr/bin/timepointer ]
 then
-echo "timepointer is aleready installed"
+	echo "timepointer is aleready installed"
 
 else
-echo "installing timepointer"
+	echo "installing timepointer"
 
-echo "creating working dir"
-mkdir ~/.timepointer
+	echo "creating working dir"
+	mkdir ~/.timepointer
+	mkdir ~/.timepointer/src
 
-echo "copying files"
-cp -rv src/ ~/.timepointer/
-cp -v help.txt ~/.timepointer/
+	echo "copying files"
+	cp -v src/*.py ~/.timepointer/src/
+	cp -v src/timepointer.sh ~/.timepointer/src/
+	cp -v help.txt ~/.timepointer/
 
-echo "creating link to command"
-sudo ln -sv ~/.timepointer/src/timepointer.sh /usr/bin/timepointer
+	echo "creating json file"
+	touch ~/.timepointer/src/counting.json
 
-echo "timepointer has been installed sucessfully"
-echo
-echo "for more information type: timepointer --help"
+	echo "writing data to json file"
+	echo '{ "start-time": "", "csv-dir": "", "status": "stopped" }' >> ~/.timepointer/src/counting.json
+
+	echo "creating symlink"
+	sudo ln -sv ~/.timepointer/src/timepointer.sh /usr/bin/timepointer
+
+	if [ -f /usr/bin/timepointer ]
+	then
+		echo
+		echo "timepointer has been installed sucessfully"
+		echo "for more information type: timepointer --help"
+	fi
 fi
